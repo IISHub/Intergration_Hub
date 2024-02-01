3fdf233e2b576b7b58514f1835b3554e3c7d342b
Thom Simbeye <thomsimbeye99@gmail.com>
2024-02-01 16:54:22 +0200

    landing

--- integrationhub/urls.py
+++ integrationhub/urls.py
@@ -3,7 +3,10 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from . import views
+
 urlpatterns = [
+    path('', views.landing, name="integration-hub"),
     path('admin/', admin.site.urls),
     path('mojaloops/', include('mojaloops.urls'))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
